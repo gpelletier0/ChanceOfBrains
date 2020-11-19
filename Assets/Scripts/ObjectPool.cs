83a77e75ff810cdf6e383c0c18b6372917e31db2
Guillaume Pelletier <gpelleti@isi-mtl.com>
2020-11-18 21:26:32 -0500

    [refactor] ObjectPool

--- Assets/Scripts/ObjectPool.cs
+++ Assets/Scripts/ObjectPool.cs
@@ -1,89 +1,80 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPool : MonoBehaviour
+public static class ObjectPooler
 {
-    private const int DEFAULT_POOL_SIZE = 10;
-    public static bool m_Expand = true;
-    public static Dictionary<string, List<GameObject>> m_ObjectPools = new Dictionary<string, List<GameObject>>();
+    #region Attributes
 
-    private static bool ExistsForPrefab(string path)
+    private const int DEFAULT_SIZE = 10;
+    private const bool m_bExpand = true;
+    private static readonly Dictionary<string, List<GameObject>> m_ObjectPools = new Dictionary<string, List<GameObject>>();
+
+
+    private static bool Exists(string path)
     {
         return m_ObjectPools.ContainsKey(path);
     }
 
-    private static bool isAvailable(GameObject obj)
+    private static bool IsAvailable(GameObject go)
     {
-        return !obj.activeSelf;
+        return !go.activeSelf;
     }
 
-    private static GameObject GetObject(string path, List<GameObject> pool)
+    public static GameObject GetPooledObject(string path, int size = DEFAULT_SIZE)
     {
-        if (!m_Expand) 
-            return null;
-
-        GameObject prefab = Resources.Load<GameObject>(path);
-        GameObject instance = Object.Instantiate(prefab) as GameObject;
-
-        pool.Add(instance);
-
-        return instance;
-    }
-
-    public static List<GameObject> CreatePool(string path, int num)
-    {
-        if (num <= 0) 
-            num = 1;
-
-        GameObject prefab = Resources.Load<GameObject>(path);
-        List<GameObject> ObjectList = new List<GameObject>();
-
-        for(int i = 0; i < num; i++)
-        {
-            GameObject instance = Instantiate<GameObject>(prefab);
-            ObjectList.Add(instance);
-
-            instance.SetActive(false);
-        }
-
-        m_ObjectPools.Add(path, ObjectList);
-
-        return ObjectList;
-    }
-
-    public static GameObject GetObject(string path, int size = DEFAULT_POOL_SIZE)
-    {
-        if (!ExistsForPrefab(path))
-        {
-            return Create(path, size);
-        }
-
-        List<GameObject> pool = m_ObjectPools[path];
-        GameObject instance = FindFirst(pool);
+        if (Exists(path))
+            return FindFirst(m_ObjectPools[path]) ?? Expand(path, m_ObjectPools[path]);
         
-        if(instance == null)
-            Create(path, size);
-
-        return instance;
+        return Create(path, size);
     }
 
-    private static GameObject Create(string path, int size = DEFAULT_POOL_SIZE)
+    private static GameObject Create(string path, int poolSize = DEFAULT_SIZE)
     {
-        CreatePool(path, size);
-        return GetObject(path);
+        CreateObjectPool(path, poolSize);
+
+        return GetPooledObject(path);
     }
 
     private static GameObject FindFirst(List<GameObject> pool)
     {
-        foreach(GameObject go in pool)
+        for (int i = 0; i < pool.Count; i++)
         {
-            if (isAvailable(go))
-                return go;
+            if (IsAvailable(pool[i]))
+                return pool[i];
         }
 
         return null;
     }
 
+    private static GameObject Expand(string path, List<GameObject> pool)
+    {
+        if (m_bExpand)
+        {
+            GameObject prefab = Resources.Load<GameObject>(path);
 
+            GameObject instance = Object.Instantiate(prefab) as GameObject;
+            pool.Add(instance);
+            return instance;
+        }
+    }
+
+    public static List<GameObject> CreateObjectPool(string path, int nb)
+    {
+        if (nb <= 0)
+            nb = 1;
+
+        GameObject prefab = Resources.Load<GameObject>(path);
+        List<GameObject> objects = new List<GameObject>();
+
+        for (int i = 0; i < nb; i++)
+        {
+            GameObject instance = Object.Instantiate<GameObject>(prefab);
+            objects.Add(instance);
+            instance.SetActive(false);
+        }
+
+        m_ObjectPools.Add(path, objects);
+
+        return objects;
+    }
 }
