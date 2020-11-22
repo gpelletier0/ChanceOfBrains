86deddb3f4f1da0633f592ff95ad410611b9946e
Guillaume Pelletier <gpelleti@isi-mtl.com>
2020-11-22 18:44:09 -0500

    [refactor] changed memeber variable names, moved death and fire logic inside the can move condition

--- Assets/Scripts/Game/Player/PlayerController.cs
+++ Assets/Scripts/Game/Player/PlayerController.cs
@@ -11,35 +11,34 @@ public class PlayerController : Singleton<PlayerController>, IDamageable
     [SerializeField] [Range(0, PlayerStats.DEFAULT_STAT)] public float m_MinJumpST;
 
     [Header("Walk/Run")]
-    [SerializeField] public float walkSpeed = 2.5f;
-    [SerializeField] public float runSpeed = 6f;
+    [SerializeField] public float m_WalkSpeed = 2.5f;
+    [SerializeField] public float m_RunSpeed = 6f;
 
     [Header("Jump")]
-    [SerializeField] public float jumpSpeed = 8.0f;
-    [SerializeField] public float gravity = 20.0f;
-    [SerializeField] public float jumpRate = 1.2f;
-    [SerializeField] public float jumpTimer;
-    [SerializeField] public float jumpStam = 10f;
+    [SerializeField] public float m_JumpSpeed = 8.0f;
+    [SerializeField] public float m_Gravity = 20.0f;
+    [SerializeField] public float m_JumpRate = 1.2f;
+    [SerializeField] public float m_JumpStam = 10f;
 
     [Header("Stamina")]
-    [SerializeField] public float staminaDepletion = 5.0f;
-    [SerializeField] public float staminaRecovery = 15.0f;
+    [SerializeField] public float m_StaminaDepletion = 5.0f;
+    [SerializeField] public float m_StaminaRecovery = 15.0f;
 
     [Header("Camera")]
-    [SerializeField] public float lookSpeed = 2.0f;
-    [SerializeField] public float lookXLimit = 60.0f;
-    [SerializeField] public Transform playerCameraParent;
+    [SerializeField] public float m_LookSpeed = 2.0f;
+    [SerializeField] public float m_LookXLimit = 60.0f;
+    [SerializeField] public Transform m_PlayerCameraParent;
 
     [Header("Weapon")]
-    [SerializeField] public M4A1 Wepon;
+    [SerializeField] public M4A1 m_Wepon;
 
-    private Vector3 m_moveDir = Vector3.zero;
-    private Vector2 m_rotation = Vector2.zero;
-
+    private Vector3 m_MoveDir = Vector3.zero;
+    private Vector2 m_Rotation = Vector2.zero;
+    
     private float m_Vertical;
     private float m_Horizontal;
-    private float m_Jump;
     private bool m_isRunning;
+    private float m_JumpTimer;
 
     private PlayerHUD m_PlayerHUD;
     private Animator m_Animator;
@@ -51,14 +50,16 @@ public class PlayerController : Singleton<PlayerController>, IDamageable
     public void Initialize()
     {
         CanMove = true;
+        
         PlayerStats.Instance.HP = m_HP;
         PlayerStats.Instance.ST = m_ST;
         PlayerStats.Instance.MinJumpST = m_MinJumpST;
-        PlayerStats.Instance.AmmoCount = Wepon.m_StartAmmo;
+        PlayerStats.Instance.AmmoCount = m_Wepon.m_StartAmmo;
     }
 
     protected override void Awake()
     {
+        //m_Wepon = GetComponent<M4A1>();
         m_CharController = GetComponent<CharacterController>();
         m_Animator = GetComponent<Animator>();
         m_PlayerHUD = GameObject.Find("PlayerHUD").GetComponent<PlayerHUD>();
@@ -70,23 +71,21 @@ public class PlayerController : Singleton<PlayerController>, IDamageable
         PlayerStats.Instance.HP = m_HP;
         PlayerStats.Instance.ST = m_ST;
         PlayerStats.Instance.MinJumpST = m_MinJumpST;
-        PlayerStats.Instance.AmmoCount = Wepon.m_StartAmmo;
+        PlayerStats.Instance.AmmoCount = m_Wepon.m_StartAmmo;
 
-        m_rotation.y = transform.eulerAngles.y;
+        m_Rotation.y = transform.eulerAngles.y;
     }
 
     private void Update()
     {
-        if(PlayerStats.Instance.HP <=0)
-        {
-            Die();
-        }
-
-        if (jumpTimer <= jumpRate)
-            jumpTimer += Time.deltaTime;
-
         if (CanMove)
         {
+            if (PlayerStats.Instance.HP <= 0)
+                Die();
+
+            if (m_JumpTimer > 0f)
+                m_JumpTimer -= Time.deltaTime;
+
             if (Input.GetButton("Fire1"))
             {
                 GiveDamage();
@@ -102,44 +101,48 @@ public class PlayerController : Singleton<PlayerController>, IDamageable
                 m_isRunning = Input.GetKey(KeyCode.LeftShift);
 
                 // Current direction/speed
-                m_Vertical = (!m_isRunning ? walkSpeed : GetRunSpeed()) * Input.GetAxis("Vertical");
-                m_Horizontal = (!m_isRunning ? walkSpeed : GetRunSpeed()) * Input.GetAxis("Horizontal");
-                m_Jump = jumpSpeed * Input.GetAxis("Jump");
+                m_Vertical = (!m_isRunning ? m_WalkSpeed : GetRunSpeed()) * Input.GetAxis("Vertical");
+                m_Horizontal = (!m_isRunning ? m_WalkSpeed : GetRunSpeed()) * Input.GetAxis("Horizontal");
+                //m_Jump = m_JumpSpeed * Input.GetAxis("Jump");
 
                 // Move direction vector
-                m_moveDir = (forward * m_Vertical) + (right * m_Horizontal);
+                m_MoveDir = (forward * m_Vertical) + (right * m_Horizontal);
 
-                if (Input.GetButton("Jump") && PlayerStats.Instance.ST >= PlayerStats.Instance.MinJumpST && jumpTimer >= jumpRate)
+                // Check for Jump Button press, player stamina and jumpTimer
+                if (Input.GetButton("Jump") &&
+                    PlayerStats.Instance.ST >= PlayerStats.Instance.MinJumpST &&
+                    m_JumpTimer <= 0)
                 {
                     // Jumped
                     m_Animator.SetBool("isJumping", true);
-                    m_moveDir.y = jumpSpeed;
-                    jumpTimer = 0f;
-                    PlayerStats.Instance.ST -= jumpStam;
+                    m_MoveDir.y = m_JumpSpeed;
+                    
+                    m_JumpTimer = m_JumpRate;
+                    PlayerStats.Instance.ST -= m_JumpStam;
                 }
 
                 // Set animation
-                m_Animator.SetFloat("Vertical", m_Vertical / runSpeed);
-                m_Animator.SetFloat("Horizontal", m_Horizontal / runSpeed);
+                m_Animator.SetFloat("Vertical", m_Vertical / m_RunSpeed);
+                m_Animator.SetFloat("Horizontal", m_Horizontal / m_RunSpeed);
             }
 
             // Character controler move
-            m_moveDir.y -= gravity * Time.deltaTime;
-            m_CharController.Move(m_moveDir * Time.deltaTime);
+            m_MoveDir.y -= m_Gravity * Time.deltaTime;
+            m_CharController.Move(m_MoveDir * Time.deltaTime);
 
             // Camera move
-            m_rotation.y += Input.GetAxis("Mouse X") * lookSpeed;
-            m_rotation.x += -Input.GetAxis("Mouse Y") * lookSpeed;
-            m_rotation.x = Mathf.Clamp(m_rotation.x, -lookXLimit, lookXLimit);
+            m_Rotation.y += Input.GetAxis("Mouse X") * m_LookSpeed;
+            m_Rotation.x += -Input.GetAxis("Mouse Y") * m_LookSpeed;
+            m_Rotation.x = Mathf.Clamp(m_Rotation.x, -m_LookXLimit, m_LookXLimit);
 
-            playerCameraParent.localRotation = Quaternion.Euler(m_rotation.x, 0.0f, 0.0f);
-            transform.eulerAngles = new Vector2(0f, m_rotation.y);
+            m_PlayerCameraParent.localRotation = Quaternion.Euler(m_Rotation.x, 0.0f, 0.0f);
+            transform.eulerAngles = new Vector2(0f, m_Rotation.y);
 
             // Stamina Degen/Regen
-            if (m_isRunning && (m_moveDir.x != 0 || m_moveDir.z != 0))
-                PlayerStats.Instance.ST = Mathf.Max(PlayerStats.Instance.ST - staminaDepletion * Time.deltaTime, 0.0f);
+            if (m_isRunning && (m_MoveDir.x != 0 || m_MoveDir.z != 0))
+                PlayerStats.Instance.ST = Mathf.Max(PlayerStats.Instance.ST - m_StaminaDepletion * Time.deltaTime, 0.0f);
             else
-                PlayerStats.Instance.ST = Mathf.Min(PlayerStats.Instance.ST + staminaRecovery * Time.deltaTime, PlayerStats.Instance.MaxST);
+                PlayerStats.Instance.ST = Mathf.Min(PlayerStats.Instance.ST + m_StaminaRecovery * Time.deltaTime, PlayerStats.Instance.MaxST);
         }
     }
 
@@ -150,12 +153,12 @@ public class PlayerController : Singleton<PlayerController>, IDamageable
 
     private float GetRunSpeed()
     {
-        return Mathf.Lerp(walkSpeed, runSpeed, PlayerStats.Instance.ST / PlayerStats.Instance.MaxST);
+        return Mathf.Lerp(m_WalkSpeed, m_RunSpeed, PlayerStats.Instance.ST / PlayerStats.Instance.MaxST);
     }
 
     public void GiveDamage()
     {
-        Wepon.Fire();
+        m_Wepon.Fire();
     }
 
     public void TakeDamage(float dmg)
