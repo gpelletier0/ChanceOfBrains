ba4bbc6794f09cedc614d6d4ff1178adbf6aa05c
Guillaume Pelletier <gpelleti@isi-mtl.com>
2020-11-22 18:45:08 -0500

    [feat] implementing ObjectPooling on Obelisk

--- Assets/Scripts/Game/GameMain.cs
+++ Assets/Scripts/Game/GameMain.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement;
 
 public class GameMain : MonoBehaviour
 {
+    private const string OBELISK_PREFAB_PATH = "Prefabs/Enemies/Obelisk";
+
+
     [Header("Drop")]
     public float m_DropHeight = 60f;
 
@@ -13,24 +16,22 @@ public class GameMain : MonoBehaviour
     public float m_ObeliskTime = 30f;
     public float m_SupplyTime = 45f;
 
-    private bool m_GameOver;
+    private bool m_isGameOver;
+    private bool m_isFirstObeliskSpawned;
 
     private SceneLoader m_SceneLoader;
-    private GameObject m_Obelisk;
-
     private List<GameObject> m_SupplyDropPrefabs = new List<GameObject>();
-    private List<GameObject> m_ObeliskList = new List<GameObject>();
     private List<GameObject> m_SpawnPoints = new List<GameObject>();
+
 
     private void Awake()
     {
         m_SceneLoader = GetComponent<SceneLoader>();
         m_SpawnPoints.AddRange(GameObject.FindGameObjectsWithTag("Spawnable"));
-        
-        m_Obelisk = Resources.Load<GameObject>("Prefabs/Enemies/Obelisk");
+
         m_SupplyDropPrefabs = Resources.LoadAll("Prefabs/Pickups", typeof(GameObject))
                                 .Cast<GameObject>()
-                                .Where(go => go.tag.Equals("SupplyDrop"))
+                                .Where(go => go.CompareTag("SupplyDrop"))
                                 .ToList();
     }
 
@@ -39,7 +40,7 @@ public class GameMain : MonoBehaviour
         if(GameSettings.Instance.m_ObeliskSpawnTime > 0)
             m_ObeliskTime = GameSettings.Instance.m_ObeliskSpawnTime;
         if(GameSettings.Instance.m_SupplySpawnTime > 0)
-            m_SupplyTime = GameSettings.Instance.SetSupplySpawnTime;
+            m_SupplyTime = GameSettings.Instance.SupplySpawnTime;
 
         PlayerController.Instance.Initialize();
 
@@ -47,20 +48,18 @@ public class GameMain : MonoBehaviour
         Cursor.visible = false;
 
         PlayerHUD.Instance.ShowObjectiveText("Destroy all Obelisks", Color.red);
-
+        
         StartCoroutine(SpawnMoreOverlords());
         StartCoroutine(SpawnMoreSupplyDrop());
     }
 
     private void Update()
     {
-        m_ObeliskList.RemoveAll(item => item == null);
-
-        if (!m_GameOver)
+        if (!m_isGameOver && m_isFirstObeliskSpawned)
         {
-            if (PlayerStats.Instance.HP <= 0 || m_ObeliskList.Count <= 0)
+            if (PlayerStats.Instance.HP <= 0 || GameObject.FindGameObjectsWithTag("Obelisk").Length == 0)
             {
-                m_GameOver = true;
+                m_isGameOver = true;
                 PlayerController.Instance.CanMove = false;
 
                 Cursor.lockState = CursorLockMode.None;
@@ -74,23 +73,30 @@ public class GameMain : MonoBehaviour
     private void SpawnObelisk()
     {
         PlayerHUD.Instance.ShowObjectiveText("Obelisk has spawned", Color.red);
-        GameObject go = Instantiate(m_Obelisk, GetRandomSpawnPoint(), Quaternion.identity);
+
+        GameObject go = ObjectPooler.GetPooledObject(OBELISK_PREFAB_PATH);
         go.GetComponent<Obelisk>().m_NbVampireBats = GameSettings.Instance.NbVampireBats;
-        m_ObeliskList.Add(go);
+        go.transform.position = GetRandomSpawnPoint();
+        
+        go.GetComponent<BoxCollider>().isTrigger = true;
+        go.GetComponent<Rigidbody>().isKinematic = false;
+        go.SetActive(true);
     }
 
     private void SpawnSupplyDrop()
     {
-
         PlayerHUD.Instance.ShowObjectiveText("Supply Drop Inbound", Color.red);
         Instantiate(m_SupplyDropPrefabs[Random.Range(0, m_SupplyDropPrefabs.Count)], GetRandomSpawnPoint(), Quaternion.identity);
     }
 
     private IEnumerator SpawnMoreOverlords()
     {
-        SpawnObelisk();
+        yield return new WaitForSeconds(PlayerHUD.Instance.m_TextDisplayTime);
 
-        while (m_ObeliskList.Count > 0)
+        SpawnObelisk();
+        m_isFirstObeliskSpawned = true;
+
+        while (true)
         {
             yield return new WaitForSeconds(m_ObeliskTime);
 
