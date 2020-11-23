672ddc54e5d3630e72a8ec90be450c86a0a4541d
Guillaume Pelletier <gpelleti@isi-mtl.com>
2020-11-22 20:20:47 -0500

    formatting

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -54,11 +54,11 @@ public abstract class Singleton<T> : MonoBehaviour where T : Component
 		if ( instance == null )
 		{
 			instance = this as T;
-			DontDestroyOnLoad (gameObject);
+			DontDestroyOnLoad(gameObject);
 		}
 		else
 		{
-			Destroy (gameObject);
+			Destroy(gameObject);
 		}
 	}
 
